1928a72dc4de67f19c9b399485bea3bb0af8b150
aaronlam <aaronlam1618@gmail.com>
2019-05-28 10:28:20 -0700

    added chat list recycler view

--- app/src/main/java/com/instantmessagingsystem/controller/MainActivity.java
+++ app/src/main/java/com/instantmessagingsystem/controller/MainActivity.java
@@ -2,16 +2,45 @@ package com.instantmessagingsystem.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.instantmessagingsystem.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements RecyclerViewAdapter.ItemClickListener {
+
+    RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        List<String> chatsListDemo = new ArrayList<>();
+        chatsListDemo.add("Chat1");
+        chatsListDemo.add("Chat2");
+        chatsListDemo.add("Chat3");
+        chatsListDemo.add("Chat4");
+        chatsListDemo.add("Chat5");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.chat_lists_recycler_view);
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerViewAdapter(this, chatsListDemo);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
