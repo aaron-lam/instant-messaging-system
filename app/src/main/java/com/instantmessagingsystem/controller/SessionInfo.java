f70b830d9efefa59b049a0f65f7bf74b1b41bbf5
sellc <chris.d.sellers@gmail.com>
2019-05-30 15:15:52 -0700

    Update SessionInfo.java
    New methods and small changes.

--- app/src/main/java/com/instantmessagingsystem/controller/SessionInfo.java
+++ app/src/main/java/com/instantmessagingsystem/controller/SessionInfo.java
@@ -1,15 +1,46 @@
 package com.instantmessagingsystem.controller;
 
+import java.util.ArrayList;
+
 public class SessionInfo {
 
-    private HashPassword hashedPassword;
+    private String username;
+    private String hashedPassword;
+    private ArrayList<Chat> chatList;
+    private int activeChat;
 
-    public SessionInfo(){
-        hashedPassword = new HashPassword();
+    public SessionInfo(String username, String hashedPassword){
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+        chatList = new ArrayList<Chat>();
+        activeChat = -1;
     }
 
-    public void hashPassword(){
-        hashedPassword.hashPassword(password);
+    public ArrayList<Chat> getChatList(){
+        return chatList;
+    }
+
+    public void setActiveChat(int index){
+        this.activeChat = index;
+    }
+
+    public int getActiveChat(){
+        return activeChat;
+    }
+
+    public void addChat(Chat newChat){
+        chatList.add(newChat);
+    }
+
+    public void removeChat(int index){
+        chatList.remove(index);
+    }
+
+    public void logout(){
+        username = "";
+        hashedPassword = "";
+        chatList = null;
+        activeChat = -1;
     }
 
 }
