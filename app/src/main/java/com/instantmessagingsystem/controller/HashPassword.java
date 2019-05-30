7c1caa67bdbab3c6870a94ac46305f0fb030d1b2
sellc <chris.d.sellers@gmail.com>
2019-05-30 16:04:41 -0700

    Update HashPassword.java
    Added SHA-256 hashing algorithm

--- app/src/main/java/com/instantmessagingsystem/controller/HashPassword.java
+++ app/src/main/java/com/instantmessagingsystem/controller/HashPassword.java
@@ -1,20 +1,40 @@
 package com.instantmessagingsystem.controller;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashPassword {
 
-    private String hashedPass;
+    //Example from https://www.geeksforgeeks.org/sha-256-hash-in-java/
+    public static String hashPassword(String password) {
+        try {
+            // Static getInstance method is called with hashing SHA
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
 
-    public HashPassword(){
-        hashedPass = "";
-    }
+            // digest() method called
+            // to calculate message digest of an input
+            // and return array of byte
+            byte[] messageDigest = md.digest(password.getBytes());
 
-    public void hashPassword(String password){
-        //hash password then set
-        hashedPass = password;
-    }
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
 
-    public String getHashedPass(){
-        return hashedPass;
+            // Convert message digest into hex value
+            String hashedValue = no.toString(16);
+
+            while (hashedValue.length() < 32) {
+                hashedValue = "0" + hashedValue;
+            }
+            return hashedValue;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            System.out.println("Exception thrown"
+                    + " for incorrect algorithm: " + e);
+            return null;
+        }
     }
 
 }
