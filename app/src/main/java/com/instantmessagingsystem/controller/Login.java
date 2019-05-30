ecb1a72fe9945b693c7c1eac578906615e5feb23
sellc <chris.d.sellers@gmail.com>
2019-05-30 14:25:18 -0700

    Update Login.java
    Small changes

--- app/src/main/java/com/instantmessagingsystem/controller/Login.java
+++ app/src/main/java/com/instantmessagingsystem/controller/Login.java
@@ -4,19 +4,16 @@ public class Login {
 
     //Check with DB if credentials are correct.
     //Hash password before sending
-    private HashPassword hashPassword;
     private DatabaseQueries dbQueries;
     private final String table = "Accounts";
 
     public Login(){
-        hashPassword = new HashPassword();
         dbQueries = new DatabaseQueries();
     }
 
-    public boolean checkCredentials(String username, String password) {
+    public boolean checkCredentials(String username, String hashedPassword) {
         //Needs to be modified
         dbQueries.get(table, username);
-        hashPassword.hashPassword(password);
         return false;
     }
 }
