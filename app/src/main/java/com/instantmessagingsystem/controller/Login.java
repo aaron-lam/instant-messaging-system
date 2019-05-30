d2d1a6f6a2ae6376c5701db311810a69b726965b
sellc <chris.d.sellers@gmail.com>
2019-05-30 16:20:38 -0700

    Update Login.java
    Moved variables to SessionInfo. Removed duplicate logic and variables.

--- app/src/main/java/com/instantmessagingsystem/controller/Login.java
+++ app/src/main/java/com/instantmessagingsystem/controller/Login.java
@@ -4,14 +4,9 @@ public class Login {
 
     //Check with DB if credentials are correct.
     //Hash password before sending
-    private DatabaseQueries dbQueries;
     private final String table = "Accounts";
 
-    public Login(){
-        dbQueries = new DatabaseQueries();
-    }
-
-    public boolean checkCredentials(String username, String hashedPassword) {
+    private boolean checkCredentials(String username, String hashedPassword) {
         //Needs to be modified
         dbQueries.get(table, username);
         return false;
