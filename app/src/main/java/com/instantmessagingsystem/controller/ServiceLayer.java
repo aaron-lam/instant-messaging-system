bcadbc41ee88a4d503de278907bf82633af7edc8
sellc <chris.d.sellers@gmail.com>
2019-06-02 12:37:42 -0700

    Revert "ServiceLayer refactor"
    This reverts commit dc0e286f93a5d03266baa34dc962c64d5ce0f0c8.

--- app/src/main/java/com/instantmessagingsystem/controller/ServiceLayer.java
+++ app/src/main/java/com/instantmessagingsystem/controller/ServiceLayer.java
@@ -1,8 +1,8 @@
 package com.instantmessagingsystem.controller;
 
 import android.content.Context;
-//import android.content.Intent;
-//import android.renderscript.ScriptGroup;
+import android.content.Intent;
+import android.renderscript.ScriptGroup;
 import android.widget.Toast;
 
 import com.instantmessagingsystem.model.entities.Chat;
@@ -17,43 +17,48 @@ public class ServiceLayer {
     private DatabaseHelper dbHelper;
     private User user;
     private InputVerification verification;
+
+    private String username;
+    private String hashedPassword;
     private ArrayList<Chat> chatList;
     private int activeChat;
+
     private Stack<String> messages;
     private String typedMessage;
 
     public ServiceLayer(Context context){
         dbHelper = new DatabaseHelper(context);
         verification = new InputVerification();
-        activeChat = -1;
-        chatList = new ArrayList<Chat>();
         messages = new Stack<String>();
         typedMessage = "";
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+        chatList = new ArrayList<Chat>();
+        activeChat = -1;
     }
 
-    public boolean createAccount(Context context, String username, String password){
-        boolean isInserted = dbHelper.insertUser(username, password);
+    public boolean createAccount(Context context, String username, String hashedPass){
+        boolean isInserted = dbHelper.insertUser(username, hashedPassword);
         String toastMessage = (isInserted) ? "account created" : "cannot create account";
         Toast.makeText(context, toastMessage, Toast.LENGTH_LONG).show();
         return false;
     }
 
-    public boolean loginUser(Context context, String username, String password){
-        if(validateCredentials(user.getUserName(), user.getPassword())) {
-            boolean isExistingUser = dbHelper.isExistingUser(username, password);
-            String toastMessage = (isExistingUser) ? "Login Successful" : "Login Unsuccessful";
-            Toast.makeText(context, toastMessage, Toast.LENGTH_LONG).show();
-            if (isExistingUser) {
-                //create user and user's chat instances
-                user = new User(username, password);
-                ArrayList<String> chatIds = dbHelper.getUserChatIds(username);
-                ArrayList<Chat> chats = new ArrayList<>();
-                for (String chatId : chatIds)
-                    chats.add(new Chat(chatId));
-                user.setChats(chats);
-                // go to main page
-                return true;
-            }
+    public boolean loginUser(Context context){
+        boolean isExistingUser = dbHelper.isExistingUser(username, hashedPassword);
+        String toastMessage = (isExistingUser) ? "Login Successful" : "Login Unsuccessful";
+        Toast.makeText(context, toastMessage, Toast.LENGTH_LONG).show();
+
+        if (isExistingUser) {
+            //create user and user's chat instances
+            user = new User(username, hashedPassword);
+            ArrayList<String> chatIds = dbHelper.getUserChatIds(username);
+            ArrayList<Chat> chats = new ArrayList<>();
+            for (String chatId : chatIds)
+                chats.add(new Chat(chatId));
+            user.setChats(chats);
+            // go to main page
+            return true;
         }
         return false;
     }
@@ -99,7 +104,8 @@ public class ServiceLayer {
         chatList.remove(index);
     }
     public void logout(){
-        user = null;
+        username = "";
+        hashedPassword = "";
         chatList = null;
         activeChat = -1;
     }
